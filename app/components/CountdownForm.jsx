fc27d417bd80f84501fc00f371fbd72d9248beb8
Lawrence Francell <lawrence@twintails.com>
2017-03-07 06:37:48 -0600

    Countdown form HTML Validation with custom message

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -5,6 +5,7 @@ class CountdownForm extends React.Component{
       super(props)
       this.onSubmit = this.onSubmit.bind(this)
     }
+
     onSubmit(e) {
       e.preventDefault()
       var strSeconds = this.refs.seconds.value
@@ -14,11 +15,16 @@ class CountdownForm extends React.Component{
       }
     }
 
+    componentDidMount() {
+      console.log('Form Did Mount');
+      console.log(this.refs.form[0].setCustomValidity("You can't coundown from nothing! That's funny and silly! -Zoë"));
+    }
+
     render() {
         return (
           <div ref={node => this.node = node}>
             <form ref="form" onSubmit={this.onSubmit} className="countdown-form">
-              <input type="text" ref="seconds" placeholder="Enter time in seconds"/>
+              <input type="text" ref="seconds" placeholder="Enter time in seconds" required="required"/>
               <button className="button expanded">Start</button>
             </form>
           </div>
