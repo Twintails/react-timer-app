d1de68edc7e8c2a195818219d43d933212410b9b
Lawrence Francell <lawrence@twintails.com>
2017-03-08 08:06:43 -0600

    merge changes to form input from stashed work

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -8,11 +8,31 @@ class CountdownForm extends React.Component{
 
     onSubmit(e) {
       e.preventDefault()
-      var strSeconds = this.refs.seconds.value
+      let strSeconds = this.refs.seconds.value
+      console.log("Form Called", this.refs);
+      console.log("input valid? ", this.refs.form[0].checkValidity());
+      if (this.refs.form[0].checkValidity() && this.refs.form[0].value === "" ) {
+        this.refs.form[0].setAttribute("required", "required")
+        this.refs.form[0].setCustomValidity("You can't coundown from nothing! That's funny and silly! -Zoë")
+      } else {
+        this.refs.form[0].removeAttribute("required")
+        this.refs.form[0].setValidationMessage = ""
+      }
       if (strSeconds.match(/^[0-9]*$/)) {
         this.refs.seconds.value = ''
         this.props.onSetCountdown(parseInt(strSeconds, 10))
       }
+    }
+
+    componentDidMount() {
+      // console.log("Mounted Form ",this.refs);
+      // this.node.children[0].children[0].setCustomValidity("You can't coundown from nothing! That's funny and silly! -Zoë")
+
+      // this.refs.form[0].setValidationMessage = "You can't coundown from nothing! That's funny and silly! -Zoë"
+    }
+
+    componentDidUpdate() {
+      // console.log('Form Updates');
     }
 
     render() {
